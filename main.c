576d5fd0f0c6693decce939b0c8a0cbaef88d145
Abhishek Sahoo <75966266+Oreek@users.noreply.github.com>
2025-12-03 19:14:07 +0530

    modified:   main.c 	modified:   text-editor 	deleted:    text-editor.exe

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <stdio.h>
-#include <X11/Xlib.h>
+#include <ctype.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    };
     return 0;
 }
