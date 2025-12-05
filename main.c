1a5942181e09deb5bb0da077c49030affcd5a572
Abhishek Sahoo <75966266+Oreek@users.noreply.github.com>
2025-12-05 20:12:44 +0530

    modified:   main.c 	modified:   text-editor

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 
 /*** Defines ***/
 
-#define KILO_TAB_STOP 8
+#define MEOW_TAB_STOP 8
 #define CTRL_KEY(k) ((k) &  0x1f)
 #define MEOWDITOR_VERSION "0.0.67"
 
@@ -44,6 +44,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rx;
     int rowoff;
     int coloff;
     int screenrows;
@@ -167,33 +168,38 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** Row Functions ***/
 
-void editorUpdateRow(erow *row) {
-    int tabs = 0;
+int editorRowCxToRx(erow *row, int cx) {
+    int rx = 0;
     int j;
-    for (j = 0; j < row->size; j++)
-        if (row->chars[j] == '\t') tabs++;
-
-    free(row->render);
-    row->render = malloc(tow->size + tabs*7 + 1);
-
-    int idx = 0;
-    for (j = 0; j < row->size; j++) {
-        if (row->chars[j] == '\t') {
-            row->render[idx++] = ' ';
-            while (idx % 8 != 0) row->render[idx++] = ' ';
-        } else {
-            row->render[idx++] = row->chars[j];
-        }
+    for (j = 0; j < cx; j++) {
+        if (row->chars[j] == '\t') rx += (MEOW_TAB_STOP - 1) - (rx % MEOW_TAB_STOP);
+        rx++;
     }
+    return rx;
+}
 
-    
-    row->render[idx] = '\0';
-    row->rsize = idx;
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t') tabs++;
+  free(row->render);
+  row->render = malloc(row->size + tabs*(MEOW_TAB_STOP - 1) + 1);
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % MEOW_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
 }
 
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
-  
   int at = E.numrows;
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
@@ -201,10 +207,9 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars[len] = '\0';
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
-  editorAppendRow(&E.row[at]);
+  editorUpdateRow(&E.row[at]);
   E.numrows++;
 }
-
 /*** File Input & Output ***/
 
 void editorOpen(char *filename) {
@@ -245,17 +250,22 @@ void abFree(struct abuf *ab) {
 /*** Output ***/
 
 void editorScroll() {
+
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff) {
-    E.coloff = E.cx;
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols) {
-    E.coloff = E.cx - E.screencols + 1;
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -292,6 +302,7 @@ void editorDrawRows(struct abuf *ab) {
   }
 }
 
+
 void editorRefreshScreen() {
     editorScroll();
     
@@ -300,7 +311,7 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H", 3);
     editorDrawRows(&ab);
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);    abAppend(&ab, buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1); abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6);
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -383,6 +394,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
