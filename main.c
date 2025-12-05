a27579536cc1ebc3e87b68f7bf60cc35d90a9902
Abhishek Sahoo <75966266+Oreek@users.noreply.github.com>
2025-12-05 18:31:57 +0530

    modified:   main.c 	modified:   text-editor

--- main.c
+++ main.c
@@ -185,9 +185,7 @@ void editorOpen(char *filename) {
   size_t linecap = 0;
   ssize_t linelen;
   while ((linelen = getline(&line, &linecap, fp)) != -1) {
-    while (linelen > 0 && (line[linelen - 1] == '\n' ||
-                           line[linelen - 1] == '\r'))
-      linelen--;
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
     editorAppendRow(line, linelen);
   }
   free(line);
@@ -282,14 +280,24 @@ void editorRefreshScreen() {
 /*** Input ***/
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
+      } else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
-        E.cx++;
+    if (row && E.cx < row->size) {
+      E.cx++;
+    } else if (row && E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
+    }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -301,6 +309,12 @@ void editorMoveCursor(int key) {
         E.cy++;
       }
       break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
